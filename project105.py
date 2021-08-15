92425468575a1bbc96e9cf2b908986d1bdaa295b
ilisha0512 <68852416+ilisha0512@users.noreply.github.com>
2021-08-15 12:14:23 +0530

    Add files via upload

---
+++ project105.py
@@ -0,0 +1,24 @@
+import math
+import csv
+with open("standard deviation.csv") as f:
+    reader = csv.reader(f)
+    filedata = list(reader)
+data = filedata[0]
+def mean(data):
+    n = len(data)
+    total = 0
+    for i in data:
+        total = total + int(i)
+    mean = total/n
+    return mean
+squaredlist = []
+for i in data:
+    a = int(i) - mean(data)
+    a = a**2
+    squaredlist.append(a)
+sum = 0
+for i in squaredlist:
+    sum = sum+i
+result = sum/(len(data)-1)
+standarddeviation = math.sqrt(result)
+print(standarddeviation)
